0ad2b1810bc044255c979e8bde03df7175458ded
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-11-06 18:57:34 +0100

    gradient coloring

--- src/shaders/grassFragment.glsl
+++ src/shaders/grassFragment.glsl
@@ -10,8 +10,15 @@ in mat4 normalMatrix;
 in vec3 vNormal1;
 in vec3 vNormal2;
 
+float easeIn(float t, float alpha) {
+    return pow(t, alpha);
+}
+
 void main() {
-    vec3 finalColor = vec3(0.0, 0.4, 0.0);
+    vec3 baseColor = vec3(0.05, 0.2, 0.01);
+    vec3 tipColor = vec3(0.5, 0.5, 0.1);
+
+    vec3 finalColor = mix(baseColor, tipColor, easeIn(vPosition.y, 4.0));
 
     float normalBlending = vPosition.x + 0.5;
 
