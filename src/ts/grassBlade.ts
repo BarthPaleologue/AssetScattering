a1dafcf853036053dc4f28aa12e40e5229fd334f
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-11-06 17:23:10 +0100

    first grass blade

---
+++ src/ts/grassBlade.ts
@@ -0,0 +1,80 @@
+import {Color3} from "@babylonjs/core/Maths/math.color";
+import {Mesh} from "@babylonjs/core/Meshes/mesh";
+import {VertexData} from "@babylonjs/core/Meshes/mesh.vertexData";
+import {Scene} from "@babylonjs/core/scene";
+import {PBRMaterial} from "@babylonjs/core/Materials/PBR/pbrMaterial";
+import {StandardMaterial} from "@babylonjs/core";
+
+export function makeGrassBlade(scene: Scene, nbStacks: number) {
+    const mesh = new Mesh("grassBlade", scene);
+
+    const nbVertices = 2 * nbStacks + 1;
+    const positions = new Float32Array(nbVertices * 3);
+    const normals = new Float32Array(nbVertices * 3);
+    const indices = new Uint32Array((2 * (nbStacks - 1) + 1) * 3);
+
+    // The vertices are aranged in rows of 2 vertices, we stack the rows on top of each other until we reach the top of the blade
+    let vertexIndex = 0;
+    let normalIndex = 0;
+    let indexIndex = 0;
+    const step = 1 / nbStacks;
+    for (let i = 0; i < nbStacks; i++) {
+        positions[vertexIndex++] = -0.1;
+        positions[vertexIndex++] = i * step;
+        positions[vertexIndex++] = 0;
+
+        positions[vertexIndex++] = 0.1;
+        positions[vertexIndex++] = i * step;
+        positions[vertexIndex++] = 0;
+
+        normals[normalIndex++] = 0;
+        normals[normalIndex++] = 0;
+        normals[normalIndex++] = 1;
+
+        normals[normalIndex++] = 0;
+        normals[normalIndex++] = 0;
+        normals[normalIndex++] = 1;
+
+        if (i === 0) {
+            continue;
+        }
+
+        // make 2 triangles out of the vertices
+        indices[indexIndex++] = 2 * (i - 1);
+        indices[indexIndex++] = 2 * (i - 1) + 1;
+        indices[indexIndex++] = 2 * i;
+
+        indices[indexIndex++] = 2 * i;
+        indices[indexIndex++] = 2 * (i - 1) + 1;
+        indices[indexIndex++] = 2 * i + 1;
+    }
+
+    // the last vertex is the tip of the blade
+    positions[vertexIndex++] = 0;
+    positions[vertexIndex++] = nbStacks * step;
+    positions[vertexIndex++] = 0;
+
+    normals[normalIndex++] = 0;
+    normals[normalIndex++] = 0;
+    normals[normalIndex++] = 1;
+
+    // last triangle
+    indices[indexIndex++] = 2 * (nbStacks - 1);
+    indices[indexIndex++] = 2 * (nbStacks - 1) + 1;
+    indices[indexIndex++] = 2 * nbStacks;
+
+    const vertexData = new VertexData();
+    vertexData.positions = positions;
+    vertexData.normals = normals;
+    vertexData.indices = indices;
+
+    vertexData.applyToMesh(mesh);
+
+    const material = new StandardMaterial("grassBladeMaterial", scene);
+    material.backFaceCulling = false;
+    material.diffuseColor = new Color3(0.5, 1, 0.5);
+
+    mesh.material = material;
+
+    return mesh;
+}
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,16 +3,17 @@ import { Scene } from "@babylonjs/core/scene";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
-import { Effect } from "@babylonjs/core/Materials/effect";
+/*import { Effect } from "@babylonjs/core/Materials/effect";
 import { PostProcess } from "@babylonjs/core/PostProcesses/postProcess";
-import { Texture } from "@babylonjs/core/Materials/Textures/texture";
+import { Texture } from "@babylonjs/core/Materials/Textures/texture";*/
 import { PointLight } from "@babylonjs/core/Lights/pointLight";
 import "@babylonjs/core/Materials/standardMaterial";
 import "@babylonjs/core/Loading/loadingScreen";
 
 import "../styles/index.scss";
+import {makeGrassBlade} from "./grassBlade";
 
-import postprocessCode from "../shaders/smallPostProcess.glsl";
+//import postprocessCode from "../shaders/smallPostProcess.glsl";
 
 const canvas = document.getElementById("renderer") as HTMLCanvasElement;
 canvas.width = window.innerWidth;
@@ -22,25 +23,23 @@ const engine = new Engine(canvas);
 
 const scene = new Scene(engine);
 
-const camera = new FreeCamera("camera", Vector3.Zero(), scene);
+const camera = new FreeCamera("camera", new Vector3(0, 6, -10), scene);
+camera.setTarget(Vector3.Zero());
 camera.attachControl();
 
 const light = new PointLight("light", new Vector3(-5, 5, 10), scene);
 
-const sphere = MeshBuilder.CreateSphere("sphere", { segments: 32, diameter: 1 }, scene);
-sphere.position = new Vector3(0, 0, 10);
+const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
 
-Effect.ShadersStore[`PostProcess1FragmentShader`] = postprocessCode;
-const postProcess = new PostProcess("postProcess1", "PostProcess1", [], ["textureSampler"], 1, camera, Texture.BILINEAR_SAMPLINGMODE, engine);
+const grassBlade = makeGrassBlade(scene, 5);
+
+//Effect.ShadersStore[`PostProcess1FragmentShader`] = postprocessCode;
+//const postProcess = new PostProcess("postProcess1", "PostProcess1", [], ["textureSampler"], 1, camera, Texture.BILINEAR_SAMPLINGMODE, engine);
 
 let clock = 0;
-
 function updateScene() {
     const deltaTime = engine.getDeltaTime() / 1000;
     clock += deltaTime;
-
-    sphere.position.x = Math.cos(clock);
-    sphere.position.z = 5 + Math.sin(clock);
 }
 
 scene.executeWhenReady(() => {
