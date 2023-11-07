80e358b061c39485ae02ed0ecd701d1c2e7ad3ee
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-11-07 15:45:15 +0100

    added back old regular instance scatterer

--- src/ts/instancePatch.ts
+++ src/ts/instancePatch.ts
@@ -1,47 +1,61 @@
-import {Matrix, Quaternion, Vector3} from "@babylonjs/core/Maths/math.vector";
+import {Vector3} from "@babylonjs/core/Maths/math.vector";
+import {Mesh} from "@babylonjs/core/Meshes/mesh";
 import {InstancedMesh} from "@babylonjs/core/Meshes/instancedMesh";
 
+function swap(oldInstance: InstancedMesh, newInstance: InstancedMesh) {
+    newInstance.position.copyFrom(oldInstance.position);
+    newInstance.rotation.copyFrom(oldInstance.rotation);
+    newInstance.scaling.copyFrom(oldInstance.scaling);
+    oldInstance.dispose();
+}
+
 export class InstancePatch {
+    private readonly meshFromLod: Mesh[];
     instances: InstancedMesh[];
     readonly position: Vector3;
     readonly size: number;
     readonly resolution: number;
-    readonly matrixBuffer: Float32Array;
     lod: number;
 
-    constructor(lod: number, patchPosition: Vector3, patchSize: number, patchResolution: number) {
-        this.instances = [];
+    constructor(baseMeshFromLOD: Mesh[], lod: number, patchPosition: Vector3, patchSize: number, patchResolution: number) {
+        this.meshFromLod = baseMeshFromLOD;
+        this.instances = InstancePatch.Scatter(baseMeshFromLOD[lod], patchPosition, patchSize, patchResolution);
         this.position = patchPosition;
         this.size = patchSize;
         this.resolution = patchResolution;
         this.lod = lod;
-        this.matrixBuffer = new Float32Array(16 * this.resolution * this.resolution);
-        this.createMatrixBuffer();
     }
 
-    public setLOD(lod: number) {
+    setLOD(lod: number) {
+        if (lod === this.lod) return;
+
+        const newInstances = [];
+        for (const instance of this.instances) {
+            const bladeType = this.meshFromLod[lod];
+            const newInstance = bladeType.createInstance(instance.name);
+            swap(instance, newInstance);
+            newInstances.push(newInstance);
+        }
+
+        this.instances = newInstances;
         this.lod = lod;
     }
 
-    private createMatrixBuffer() {
-        const cellSize = this.size / this.resolution;
-        let index = 0;
-        for (let x = 0; x < this.resolution; x++) {
-            for (let z = 0; z < this.resolution; z++) {
+    static Scatter(baseMesh: Mesh, patchPosition: Vector3, patchSize: number, patchResolution: number) {
+        const instances = [];
+        const cellSize = patchSize / patchResolution;
+        for (let x = 0; x < patchResolution; x++) {
+            for (let z = 0; z < patchResolution; z++) {
+                const instance = baseMesh.createInstance(`blade${x}${z}`);
                 const randomCellPositionX = Math.random() * cellSize;
                 const randomCellPositionZ = Math.random() * cellSize;
-                const positionX = this.position.x + x * cellSize - (this.size / 2) + randomCellPositionX;
-                const positionZ = this.position.z + z * cellSize - (this.size / 2) + randomCellPositionZ;
-
-                const matrix = Matrix.Compose(
-                    new Vector3(1, 1, 1),
-                    Quaternion.RotationAxis(Vector3.Up(), Math.random() * 2 * Math.PI),
-                    new Vector3(positionX, 0, positionZ)
-                );
-                matrix.copyToArray(this.matrixBuffer, 16 * index);
-
-                index += 1;
+                instance.position.x = patchPosition.x + (x / patchResolution) * patchSize - patchSize / 2 + randomCellPositionX;
+                instance.position.z = patchPosition.z + (z / patchResolution) * patchSize - patchSize / 2 + randomCellPositionZ;
+                instance.rotation.y = Math.random() * 2 * Math.PI;
+                instances.push(instance);
             }
         }
+
+        return instances;
     }
 }
--- src/ts/instanceScatterer.ts
+++ src/ts/instanceScatterer.ts
@@ -18,38 +18,20 @@ export class InstanceScatterer {
         this.radius = radius;
         this.computeLodLevel = computeLodLevel;
 
-        const matricesByLod: Float32Array[][] = [];
-        for(let lod = 0; lod < this.meshFromLod.length; lod++) {
-            matricesByLod.push([]);
-        }
-        const lods = new Set<number>();
-
         for (let x = -this.radius; x <= this.radius; x++) {
             for (let z = -this.radius; z <= this.radius; z++) {
                 const radiusSquared = x * x + z * z;
                 if (radiusSquared > this.radius * this.radius) continue;
 
                 const patchPosition = new Vector3(x * patchSize, 0, z * patchSize);
-                const patch = new InstancePatch( 1, patchPosition, patchSize, patchResolution);
-                patch.setLOD(this.computeLodLevel(patch));
-
-                matricesByLod[patch.lod].push(patch.matrixBuffer);
-                lods.add(patch.lod);
+                const patch = new InstancePatch(this.meshFromLod, 0, patchPosition, patchSize, patchResolution);
 
                 this.map.set(patchPosition, patch);
             }
         }
-
-        this.updateLodMatrices(lods, matricesByLod);
     }
 
     update() {
-        const matrixBuffersByLod: Float32Array[][] = [];
-        for(let lod = 0; lod < this.meshFromLod.length; lod++) {
-            matrixBuffersByLod.push([]);
-        }
-
-        const changedLODs: Set<number> = new Set();
         for (const patchPosition of this.map.keys()) {
             const patch = this.map.get(patchPosition);
             if (!patch) {
@@ -57,33 +39,7 @@ export class InstanceScatterer {
             }
 
             const newLod = this.computeLodLevel(patch);
-            if(newLod !== patch.lod) {
-                changedLODs.add(newLod);
-                changedLODs.add(patch.lod);
-            }
             patch.setLOD(newLod);
-
-            matrixBuffersByLod[patch.lod].push(patch.matrixBuffer);
-        }
-
-        this.updateLodMatrices(changedLODs, matrixBuffersByLod);
-    }
-
-    private updateLodMatrices(lods: Set<number>, matrixBuffersByLod: Float32Array[][]) {
-        for(const lod of lods) {
-            const mesh = this.meshFromLod[lod];
-            const matrixBuffers = matrixBuffersByLod[lod];
-
-            // concatenate all the matrix buffers in one buffer
-            const totalLength = matrixBuffers.length > 0 ? matrixBuffers.length * matrixBuffers[0].length : 0;
-            const finalMatrixBuffer = new Float32Array(totalLength);
-            let offset = 0;
-            for (const matrixBuffer of matrixBuffers) {
-                finalMatrixBuffer.set(matrixBuffer, offset);
-                offset += matrixBuffer.length;
-            }
-
-            mesh.thinInstanceSetBuffer("matrix", finalMatrixBuffer, 16);
         }
     }
 }
